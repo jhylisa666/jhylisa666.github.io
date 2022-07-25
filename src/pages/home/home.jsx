9ef03e05c34d7b7275f65f197d6a69adfaa81547
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2022-07-25 15:01:47 -0400

    added github links and linkedin

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.scss'
 import ResponsiveAppBar from '../../components/appBar.js'
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
@@ -13,22 +14,30 @@ export default function Home() {
             HELLO!
           </h1>
           <body>
-            I am a second year Honors Northeastern student with a major in Computer Science and concentration 
-            in Artificial Intelligence. I am eager to pursue anything related to software development, machine learning, or artificial intelligence. 
+            I am a second year Honors Northeastern student with a major in Computer Science and concentration
+            in Artificial Intelligence. I am eager to pursue anything related to software development, machine learning, or artificial intelligence.
             <br></br><br></br>
             As a software engineering working for multiple on-campus organizations, I have lots of experience with full-stack web development as well as
             machine learning and artifical intellience.
             <br></br><br></br>
             See my work under Projects.
+            <br></br><br></br>
+            Email: jiang.hai@northeastern.edu
           </body>
         </div>
         <div className="middlePanel">
           <div className="imgContainer">
-            <img src="headshot.jpg" alt="" />
+            <img src={require("./headshot.jpg")} alt="" />
           </div>
         </div>
         <div className="linksContainer">
-          HI
+          <img src={require("./GitHubLogo.png")} alt="" />
+          <div className="LinkedInLogo">
+            <img src={require("./linkedinLogo.png")} alt="" />
+          </div>
+          <div className="buttons">
+          <Button size = "small" color = "secondary" variant="contained">resume</Button>
+          </div>
         </div>
       </div>
     </div>
